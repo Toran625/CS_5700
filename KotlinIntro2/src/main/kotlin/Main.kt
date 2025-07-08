3253454663ced927016ddfd821fae0ecfa9744b1
Ryan Austin <mewtwo625@gmail.com>
2025-07-07 18:40:39 -0600

    Removed some files

--- KotlinIntro2/src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val person = Person(null)
-
-    print("Welcome ${person.name ?: "Anonymous User"}")
-
-    var nextJobId = (person.jobId ?: 999) + 1
-}
-
-fun doMath(a: Int, b:Int) = a + b
--- KotlinIntro2/src/main/kotlin/Person.kt
+++
@@ -1,6 +0,0 @@
-class Person(var name: String?) {
-
-    val age: Int = 10
-
-    val jobId: Int? = null
-}
